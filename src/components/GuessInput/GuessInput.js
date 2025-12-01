ff64dc8a6333d70087254ffa2b0e5df575603c1d
Tommy Bui Nguyen <tommynguyen0512@gmail.com>
2025-12-01 10:20:32 -0600

    exercise 1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function GuessInput() {
+  let [guess, setGuess] = React.useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("GUESS:", guess);
+    setGuess("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        id="guess-input"
+        type="text"
+        value={guess}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
